00d0c8124418ba28417be7da32d9e13ae5abe92c
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2023-05-13 23:43:25 +0200

    add dark souls three page

---
+++ next-app/helper/darkSouls3DataHelper.ts
@@ -0,0 +1,14 @@
+import bossesJson from '../public/data/darkSouls3Bosses.json';
+import { IBoss } from '../types';
+
+/**
+ * Retrieve all available bosses in Dark Souls 3.
+ *
+ * @returns {IBoss[]} The list of bosses in Dark Souls 3.
+ */
+const getBosses = (): IBoss[] => {
+    const bosses: IBoss[] = bossesJson as IBoss[];
+    return bosses;
+};
+
+export { getBosses };
