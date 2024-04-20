2da494d1ec5df30b289ef0cc19439882a2ae3ef7
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2024-04-20 20:29:17 +0200

    add the types

---
+++ next-app/src/types/index.ts
@@ -0,0 +1,7 @@
+import { IBoss } from './boss';
+import { IEldenRingBoss } from './eldenRing/boss';
+import { INextPageProps } from './nextPageProps';
+import { IRegion } from './region';
+import { TailwindSize } from './tailwindSize';
+
+export type { IBoss, IEldenRingBoss, INextPageProps, IRegion, TailwindSize };
