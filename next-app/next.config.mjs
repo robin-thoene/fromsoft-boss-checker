29295fb4cf424e0015865ec7e0eadbace4e68dad
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2024-04-20 20:25:15 +0200

    re-add some config files

--- next-app/next.config.mjs
+++ next-app/next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/dark-souls-1',
+                permanent: true,
+            },
+        ];
+    },
+};
 
 export default nextConfig;
---
+++ next-app/src/components/atoms/button.tsx
@@ -0,0 +1,9 @@
+import { ReactElement } from 'react';
+
+/**
+ * Basic button component.
+ * @returns {ReactElement} The button element.
+ */
+export default function Button(): ReactElement {
+    return <button>TEST</button>;
+}
