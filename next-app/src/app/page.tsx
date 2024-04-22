cfcb5b6d9b01662045213cb89c1b5030d5555a55
Robin Thöne <110201991+robin-thoene@users.noreply.github.com>
2024-04-22 21:04:26 +0200

    rm no longer needed files

--- next-app/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactElement } from 'react';
-
-/**
- * Default root page to render at "/", not relevant because of permanent redirect.
- * @returns {ReactElement} Component to render at the application root.
- */
-export default function Page(): ReactElement {
-    return <></>;
-}
